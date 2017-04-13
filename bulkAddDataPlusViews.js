b5992aeee4676049d46b8145714692c810f32f86
tripott <tripott@gmail.com>
2017-04-13 09:00:52 -0400

    adding bulkAddDataPlusViews.js script to load docs and design docs (views/queries/indexes)

--- bulkAddDataPlusViews.js
+++ bulkAddDataPlusViews.js
@@ -18,7 +18,10 @@ let items = [
     "bolts",
     "hyperdrive"
   ],
-  "cost": "10000"
+  "cost": "15000",
+  "energyStarRating": 99,
+  "dateAdded": "2016-04-05T17:18Z",
+  "mfg_id": "mfg_super_inc"
 },
 {
   "_id": "item_phantom_widget_11112",
@@ -29,11 +32,14 @@ let items = [
   "components": [
     "flux capacitor"
   ],
-  "cost": "15000"
+  "cost": "15000",
+  "energyStarRating": 99,
+  "dateAdded": "2016-04-05T08:10Z",
+  "mfg_id": "mfg_super_inc"
 },
 {
   "_id": "item_super_widget_11111",
-  
+
   "type": "item",
   "name": "super widget",
   "partNumber": "11111",
@@ -41,7 +47,37 @@ let items = [
     "hyperdrive",
     "flux capacitor"
   ],
-  "cost": "20000"
+  "cost": "20000",
+  "energyStarRating": 98,
+  "dateAdded": "2016-04-01T15:34Z",
+  "mfg_id": "mfg_super_inc"
+},
+{
+  "_id": "item_crazy_widget_11113",
+
+  "type": "item",
+  "name": "crazy widget",
+  "partNumber": "11113",
+  "components": [
+    "hyperdrive",
+    "crazy sauce"
+  ],
+  "cost": "3000",
+  "energyStarRating": 78,
+  "dateAdded": "2016-04-12T11:34Z",
+  "mfg_id": "mfg_crazy_inc"
+},
+{
+  "_id": "mfg_crazy_inc",
+  "type": "mfg",
+  "name": "crazy inc",
+  "hq_state": "SC"
+},
+{
+  "_id": "mfg_super_inc",
+  "type": "mfg",
+  "name": "super inc",
+  "hq_state": "NY"
 }
 ]
 
@@ -72,8 +108,8 @@ db.bulkDocs(items, function(err, response) {
 // and outputs (emit) key value pairs.
 // Do NOT emit() the full document in your map/reduce functions.
 // Instead, use {include_docs: true} when you query the view
-const design_partsByPartNumber = {
-    _id: "_design/partsByPartNumber",
+const design_itemByPartNumber = {
+    _id: "_design/itemByPartNumber",
     language: "javascript",
     views: {
         partsByPartNumber: {
@@ -85,7 +121,109 @@ const design_partsByPartNumber = {
         }
     }
 }
-addView(design_partsByPartNumber);
+addView(design_itemByPartNumber);
+
+///////////////////////////////////////////////////////////////
+//        items sorted by cost then name
+///////////////////////////////////////////////////////////////
+// use complex keys to sort query results by cost then name
+// note the names of the design document and view are the SAME
+// recommend that you create one view per design doc,
+// and use the same name for both, in order to make things simpler.
+const design_itemCostName = {
+    _id: "_design/itemCostName",
+    language: "javascript",
+    views: {
+        partsByPartNumber: {
+            map: function(doc) {
+              if (doc.type === "item") {
+                emit([doc.cost, doc.name], 0);
+              }
+            }.toString()
+        }
+    }
+}
+addView(design_itemCostName);
+
+
+const itemsByDate = {
+    _id: "_design/itemsByDate",
+    language: "javascript",
+    views: {
+        itemsByDate: {
+            map: function(doc) {
+                if (doc.type === "item") {
+                    const dateAdded = new Date(doc.dateAdded)
+                    emit([dateAdded.getUTCFullYear(),
+                        dateAdded.getUTCMonth() + 1,
+                        dateAdded.getUTCDate(),
+                        dateAdded.getUTCHours(),
+                        dateAdded.getUTCMinutes()
+                      ],null);
+                }
+            }.toString()
+        }
+    }
+}
+addView(itemsByDate);
+
+
+///////////////////////////////////////////////
+//       Return mfg and items.
+///////////////////////////////////////////////
+// View functions specify a key and a value to be returned for each row.
+// CouchDB collates (sorts) the view rows by this key.
+// The key is composed of a mfg _id and a sorting token.
+// Because the first key value is the mfg id,
+// all mfg and items docs will be sorted by mfg.
+// Because the second key value (aka sorting token) for mfg (0) is lower than the token for items (1),
+// the mfg document will come before the associated items.
+// The values 0 and 1 for the sorting token are arbitrary.
+const designDocItemsAndMfg = {
+    _id: "_design/itemsAndMfg",
+    language: "javascript",
+    views: {
+        itemsAndMfg: {
+            map: function(doc) {
+                if (doc.type == "mfg") {
+                    emit([doc._id, 0], null);
+                } else if (doc.type == "item") {
+                    emit([doc.mfg_id, 1], null);
+                }
+            }.toString()
+        }
+    }
+}
+addView(designDocItemsAndMfg);
+
+
+/////////////////////
+//   DESIGN DOCS (QUERIES/VIEWS/INDEXES)
+/////////////////////
+// To create views in CouchDB,  you create a function that takes a document
+// and outputs (emit) key value pairs.
+// Do NOT emit() the full document in your map/reduce functions.
+// Instead, use {include_docs: true} when you query the view
+const design_components = {
+    _id: "_design/components",
+    language: "javascript",
+    views: {
+        components: {
+            map: function(doc) {
+              if(doc.type === "item" && doc.components) {
+                  doc.components.forEach(function(component) {
+                    emit(component, 1);
+                  });
+                }
+            }.toString()
+        }
+    }
+}
+addView(design_components);
+
+
+
+
 
 
 /////////////////////
